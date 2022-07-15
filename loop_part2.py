30fbf7675e46e27462b0469eb1c9eb795f85efd8
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-15 12:23:41 +0530

    Add files via upload

---
+++ loop_part2.py
@@ -0,0 +1,34 @@
+# p=int(input("enter the integer value:"))
+# for r in range(p):      # here intput taking as 5
+#     print(r)                           #  0     1     2     3     4
+#     print(r)                           #  0     1     2     3     4
+#     print(r)                           #  0     1     2     3     4
+#     print("loop has been executed")
+ 
+# n=int(input("enter the integer value:"))
+# for p in range(1,n):       #  input is 4  and the output will be in the vertical form 1 2 3
+#     print(p)
+
+# p=int(input("enter the integer value:"))
+# for r in range(12,p):                         #  input is 12  and here we using end so output will be in horizontal form
+#     print(r,end=' ')                        #  12 13 14 15 16 17 18 19
+
+# p=int(input("enter the integer value:"))
+# for r in range(3,p,3):                         # input is 10
+#     print(r)                                    # 3 6 9
+
+# p=int(input("enter the integer value:"))
+# if(p%3==0):                                 # if we give input even number then it print even value orelse if we give input odd value then it print odd number
+#     print( f"{p} even number")     
+# else:
+#     print(f"{p} odd number")    
+
+# for p in range(4):
+#     print(p*p)      #  p*p represents as power square  
+#     print(p*p*p)    #  p*p*p represents as power cube 
+
+# p=int(input("enter the integer value:"))
+# while(p<=8):           # we have to give the variable on left or right side value, otherwise it will give the infinite times as ouput
+#     print("prava",str(p))
+#     p+=1  
+            
