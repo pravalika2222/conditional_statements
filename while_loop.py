1f104faee7acf653e337991bfb7efce72fbde8f0
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-16 21:20:48 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,13 @@
+# i=int(input("enter any start value:"))   # 2
+# k=int(input("enter the end value:"))     # 10
+# while(i<=k):       # the o/p will be in the vertical form and it get in to 2-10 like(2 3 4 5 6 7 8 9 10)
+    # print(i)
+    # i+=1
+
+# i=int(input("enter any start value:"))     # 20
+# k=int(input("enter the end value:"))       # 10
+# while(i>=k):       # the o/p will be in the vertical form and it get in to 20-10 like(20 19 18 17 16 15 14 13 12 11 10)
+#     print(i)
+#     i-=1
+
+
