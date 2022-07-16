c5fe358aae85e1c3622509f4a3e905e2d5d11d36
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-16 21:20:05 +0530

    Add files via upload

---
+++ loop_part1.py
@@ -0,0 +1,36 @@
+# a=1,2,3,4,5
+# print(*a)                  # 1 2 3 4 5           
+
+# a=1,2,3
+# for p in a:
+#     print(p,end=" ")        # 1 2 3
+
+# for i in range(5):
+    # print(i)               # the ouput will get in vertical 0 1 2 3 4
+
+# n=int(input("enter the value of n:"))
+# for i in range (1,n+1):         # n=5(input)
+#     print(i,end="\n")           #  the o/p will be in the form of vertical 1 2 3 4 5
+
+# for i in range(1,30,2):    # the vlaue get in odd numbers and in horizontal form
+#     print(i,end=' ') # 1 3 7 9 11 13 15 17 19 21 23 25 27 29    
+
+# for i in range(2,30,2):   # the value get in even numbers and in horizontal form
+#     print(i,end=' ')    # 2 4 6 8 10 12 14 16 18 20 22 24 26 28
+
+# for i in range(10,0,-1):
+#     print(i)    # the output will get in the vertical form and 10 to 1 like(10 9 8 7 6 5 4 3 2 1)
+
+# for i in range(10,-1,-1):
+#     print(i)     # the output will get in the vertical form and 10 to 0 like (10 9 8 7 6 5 4 3 2 1 0)
+
+# n=[1,2.6,'prava']      # prava
+# for i in n[::-1]:      # 2.6      # this same as like slicing negative rule
+#     print(i)           # 1     output will get in the vertical form
+
+
+# n='pravalika'
+# for i in n[::2]:       # this is same as like slicing positive rule
+#     print(i)           # p a a i a     it is in vertical form
+
+
