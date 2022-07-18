30a3b2395cf9e8f7f6bf42ee1ecc96623f6c60a2
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-18 19:54:47 +0530

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,30 @@
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# a,b=[int(x) for x in input().split()]
+# print(a,b)
+
+# a=int(input("enter the value of a:"))
+# if(a==5):
+    # print("yes equal")
+    
+# a,b=[int(x) for x in input().split()]
+# if(a<b):
+    # print(f"{a}>{b} is {a>b}")
+# else:
+    # print(f"{a}<{b} is {a<b}")
+
+# a,b=[int(x) for x in input().split()]
+# if(a>b):print("a is greater than b")
+# if(a<b):print("a is less than b")
+# if(a>=b):print("a is greater than or equal to b")
+# if(a<=b):print("a is less than or equal to b")
+# if(a!=b):print("a is not equal to b")
+# if(a==b):print("a is equal to b")
+
+# a,b=[int(x) for x in input().split()]
+# if(a>b):print("a is greater than b")
+# elif(a<b):print("a is less than b")
+# elif(a>=b):print("a is greater than or equal to b")
+# elif(a<=b):print("a is less than or equal to b")
+# elif(a!=b):print("a is not equal to b")
+# elif(a==b):print("a is equal to b")
